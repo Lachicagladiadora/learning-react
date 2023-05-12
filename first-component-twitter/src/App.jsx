8101344e47ed81204dc4f962237c437c30cdc8f1
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-12 17:24:08 -0500

    twitter follow card complete

--- first-component-twitter/src/App.jsx
+++ first-component-twitter/src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+
+
 export function App () {
     return(
-        <>
-            <section className='App'>
-                <TwitterFollowCard isFollowing={true} userName="jf56" name="Juana Fernanda Estrada" urlImage="https://cdn-icons-png.flaticon.com/512/2810/2810670.png"/>
-                <TwitterFollowCard isFollowing={false} userName="rb89" name="Roberto Barrios" urlImage="https://cdn-icons-png.flaticon.com/512/6075/6075889.png"/>
-                <TwitterFollowCard isFollowing={false} userName="alb123" name="Alberto Ramos" urlImage="https://cdn-icons-png.flaticon.com/512/1754/1754806.png"/>
-                <TwitterFollowCard isFollowing userName="MJ" name="Michael Jackson"/>
-            </section>
-        </>
+
+        <section className='App'>
+            <TwitterFollowCard userName="jf56" name="Juana Fernanda Estrada" urlImage="https://cdn-icons-png.flaticon.com/512/2810/2810670.png"/>
+            <TwitterFollowCard userName="rb89" name="Roberto Alejandro Estrada" urlImage="https://cdn-icons-png.flaticon.com/512/6075/6075889.png"/>
+            <TwitterFollowCard userName="alb123" name="Alberto Ramos" urlImage="https://cdn-icons-png.flaticon.com/512/1754/1754806.png"/>
+            <TwitterFollowCard userName="MJ" name="Michael Jackson"/>
+        </section>
+
     )
 }
+
+// ANOTHER WAY OF RENDERING
+// export function App () {
+//     const pheralb = {isFollowing:true, userName: 'pheralb', name:'Alonso Eduardo Mejia', urlImage:'https://cdn-icons-png.flaticon.com/512/6076/6076493.png'}
+//     return(
+//         <section className='App'>
+//             <TwitterFollowCard {...pheralb}/>
+//         </section>
+//     )
+// }
--- first-component-twitter/src/TwitterFollowCard.jsx
+++ first-component-twitter/src/TwitterFollowCard.jsx
@@ -1,5 +1,25 @@
-export function TwitterFollowCard ({userName, name, urlImage, isFollowing}) {
+import { useState } from "react"
+
+export function TwitterFollowCard ({userName, name: children, urlImage}) {
+
+    // const state = useState(false)
+    // const isFollowing = state[0]
+    // const setIsFollowing = state[1]
+
+    // 
+    const [isFollowing, setIsFollowing] = useState(false)
+
     console.log(isFollowing)
+
+    const text = isFollowing ? 'Following' : 'Follow'
+    const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         
         <article className="tw-followCard">
@@ -9,14 +29,14 @@ export function TwitterFollowCard ({userName, name, urlImage, isFollowing}) {
                     src={urlImage} 
                     alt="avatar-1" />
                 <div className="tw-followCard-info">
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className="tw-followCard-infoUserName">@{userName}</span>
                 </div>
             </header>
 
             <aside>
-                <button className="tw-followCard-button">
-                    Follow
+                <button className={buttonClassName} onClick = {handleClick}> 
+                    {text}
                 </button>
             </aside>
         </article>
