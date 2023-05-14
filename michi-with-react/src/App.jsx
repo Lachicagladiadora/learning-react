ac6ff69a02e266583ef59262a4f764253929b2e5
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-13 22:47:29 -0500

    add turn function

--- michi-with-react/src/App.jsx
+++ michi-with-react/src/App.jsx
@@ -24,6 +24,18 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+  [0,1,2],
+]
+
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
@@ -31,14 +43,39 @@ function App() {
   console.log(board)
 
   const [turn, setTurn] = useState(TURNS.X)
+  // null: there is no winner / false: there is a tie 
+  const [state, setState] = useState(null) 
+
+  const checkWinner = (boardToCheck) => {
+    // we review all winning combinations
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if (boardToCheck[a] &&  // [0] -> X u O
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]
+        ) {
+          return boardToCheck[0] // X u O
+        }
+    }
+    // if there is no winner
+    return null 
+  }
 
   const updateBoard = (index) => {
+    //  If (you already have something written in that position) ? we do not update : we update
+    if (board[index] || winner) return
+    // update dashboard
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    // change the turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // We check if there is a winner
+    const newWinner = checkWinner(newBoard)
+    if (winner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
